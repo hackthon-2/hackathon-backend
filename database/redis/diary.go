d7b5cab77d017d79117db7966304ffeed5d48bdc
SnowWarri0r <gerrytranCHINA@gmail.com>
2021-04-14 16:02:21 +0800

    ✒完成了diary表的缓存机制的函数

--- database/redis/diary.go
+++ database/redis/diary.go
@@ -2,17 +2,34 @@ package database
 
 import (
 	"encoding/json"
+	"errors"
 	"hackthon/database"
 	"hackthon/model"
 	"strconv"
+	"time"
 )
+
+var NullPointer = errors.New("null pointer")
 
 // CreateDiaryCache 刷新缓存机制
 func CreateDiaryCache(diary *[]model.Diary) error {
+	if diary == nil {
+		return NullPointer
+	}
 	val, err := json.Marshal(diary)
 	if err != nil {
 		return err
 	}
 	conn, ctx := database.Redis()
-	return conn.HSet(ctx, "USER_"+strconv.Itoa(int((*diary)[0].UserID)), (*diary)[0].Time, string(val)).Err()
+	err = conn.HSet(ctx, "USER_"+strconv.Itoa(int((*diary)[0].UserID)), (*diary)[0].Time, string(val)).Err()
+	if err != nil {
+		return err
+	}
+	return conn.Expire(ctx, "USER_"+strconv.Itoa(int((*diary)[0].UserID)), time.Hour*72).Err()
+}
+
+// FindDiaryCache 查找缓存，没有就去数据库找，找了之后创建缓存
+func FindDiaryCache(userId uint, date string) (string, error) {
+	conn, ctx := database.Redis()
+	return conn.HGet(ctx, "USER_"+strconv.Itoa(int(userId)), date).Result()
 }
